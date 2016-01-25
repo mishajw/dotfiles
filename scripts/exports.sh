95abefb5b52ce2133e9a60ba7a8d461c03472050
Misha wagner <mishajw@gmail.com>
2016-01-25 19:22:46 +0000

    Exports

---
+++ scripts/exports.sh
@@ -0,0 +1,10 @@
+export MAIN_FONT="Monospace:style=Bold:size=11"
+export BAR_HEIGHT=30
+
+export PANEL_PATH="/home/misha/scripts/panel"
+export PANEL_FIFO="/tmp/panel-fifo"
+export VOL_CHANGED_FIFO="/tmp/vol-changed-fifo"
+
+export LANGUAGE="$LANG"
+export LC_ALL="$LANG"
+
