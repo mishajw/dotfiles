4ce1350d302861521bb165a2b5700ba7699ddd5e
Misha wagner <mishajw@gmail.com>
2016-01-26 18:12:04 +0000

    Moved and improved sh startip

---
+++ scripts/sh-startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /home/misha/scripts/exports.sh
+source /home/misha/scripts/aliases.sh
+source /home/misha/scripts/java_nonreparenting_wm_hack.sh
+
--- scripts/sh_startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PATH=$PATH:/usr/bin/core_perl
-
