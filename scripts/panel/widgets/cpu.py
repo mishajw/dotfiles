933f5390b1af515b54b3cb9ed4bba929aa112eaf
Misha wagner <mishajw@gmail.com>
2016-02-01 16:33:31 +0000

    Clicking on CPU opens htop

--- scripts/panel/widgets/cpu.py
+++ scripts/panel/widgets/cpu.py
@@ -6,6 +6,8 @@ import time
 import re
 from panelParams import SLEEP_CPU
 
+command = 'urxvt -e htop'
+
 def main():
   while(1):
     p = os.popen("mpstat")
@@ -13,7 +15,7 @@ def main():
     idle = stats[-7:]
     try:
       used = 100 - float([re.split(' +', line)[-1] for line in stats.split('\n') if "all" in line][0])
-      print("\uf017 %.2f%%" % used)
+      print("%%{A:%s:}\uf017 %.2f%%%%{A}" % (command, used))
       sys.stdout.flush()
     except:
       pass
