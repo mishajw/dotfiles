a769de3dc3bb8279637ceecb01ea799e1412e3c6
Misha wagner <mishajw@gmail.com>
2016-02-04 17:31:48 +0000

    Clicking works when internet's off too

--- scripts/panel/widgets/ipaddr.sh
+++ scripts/panel/widgets/ipaddr.sh
@@ -4,7 +4,7 @@ source $PANEL_PATH/widgets/panelParams.py
 
 while true; do
   if [[ "$(ip addr show dev wlp4s0 | grep DOWN)" ]]; then
-    echo -e '\uf119'
+    echo -e "%{A:urxvt -e bash -c \"sudo systemctl restart NetworkManager.service\":}\uf119%{A}"
   else
     ip addr show dev wlp4s0 | grep 'inet ' | awk '{print $2}' | awk -F '/' '{print "%{A:urxvt -e bash -c \"sudo systemctl restart NetworkManager.service\":}" $1 "%{A}"}'
   fi
