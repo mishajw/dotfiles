33435e0e965da60560fdf6e0cb55329183855f67
Misha wagner <mishajw@gmail.com>
2016-02-04 17:29:39 +0000

    Clicking ip address restarts internet

--- scripts/panel/widgets/ipaddr.sh
+++ scripts/panel/widgets/ipaddr.sh
@@ -6,7 +6,7 @@ while true; do
   if [[ "$(ip addr show dev wlp4s0 | grep DOWN)" ]]; then
     echo -e '\uf119'
   else
-    ip addr show dev wlp4s0 | grep 'inet ' | awk '{print $2}' | awk -F '/' '{print $1}'
+    ip addr show dev wlp4s0 | grep 'inet ' | awk '{print $2}' | awk -F '/' '{print "%{A:urxvt -e bash -c \"sudo systemctl restart NetworkManager.service\":}" $1 "%{A}"}'
   fi
 
   sleep $SLEEP_IPADDR
