a9fe8ee2905b047159b05dbd2a55794b37613cbe
Misha wagner <mishajw@gmail.com>
2016-02-05 15:13:52 +0000

    SVN editor defined

--- scripts/exports.sh
+++ scripts/exports.sh
@@ -4,9 +4,13 @@ export BAR_HEIGHT=30
 export PANEL_PATH="/home/misha/scripts/panel"
 export PANEL_FIFO="/tmp/panel-fifo"
 export VOL_CHANGED_FIFO="/tmp/vol-changed-fifo"
+export PANEL_BATTERY_FIFO="/tmp/battery-fifo"
 
 export LANGUAGE="$LANG"
 export LC_ALL="$LANG"
+
+# SVN
+export SVN_EDITOR="gvim"
 
 export PATH=$PATH:/usr/bin/core_perl
 
