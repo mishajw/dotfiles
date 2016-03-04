9421f7c50d0350397dbb77df60c36830ecee6f49
Misha Wagner <mishajw@gmail.com>
2016-03-04 10:26:40 +0000

    Changed IP (should really make it static...)

--- setup-wd-mount.sh
+++ setup-wd-mount.sh
@@ -2,5 +2,5 @@
 
 sudo mkdir -p /mnt/mycloud
 
-sudo mount -t cifs -o user=misha,passwd=,rw,file_mode=0777,dir_mode=0777 //192.168.0.37/misha /mnt/mycloud
+sudo mount -t cifs -o user=misha,passwd=,rw,file_mode=0777,dir_mode=0777 //192.168.0.2/misha /mnt/mycloud
 
