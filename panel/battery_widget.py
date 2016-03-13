afc91d4d7ca357f63316e2a33d4fe884c63eadf0
Misha Wagner <mishajw@gmail.com>
2016-03-13 17:01:02 +0000

    Battery goes red on low, exciting stuff

--- panel/battery_widget.py
+++ panel/battery_widget.py
@@ -21,17 +21,11 @@ class BatteryWidget(NumberWidget):
     self.state = battery[0][-1]
     self.number = int(battery[2][-1][:-1])
  
-  """
   def update_char(self):
-    if self.number > 66:
-      self.character = '\uf004\uf004\uf004'
-    elif self.number > 33:
-      self.character = '\uf004\uf004\uf08a'
-    elif self.number > 10:
-      self.character = '\uf004\uf08a\uf08a'
+    if self.number < 20:
+      self.character = '%{F#FF0000}V%'
     else:
-      self.character = '\uf08a\uf08a\uf08a'
-  """
+      self.character = 'V'
 
   def get_battery_dump(self):
     f = os.popen(self.battery_command)
