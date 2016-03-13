ed7d3313cb234f5c306d5462f905ac283ecb256c
Misha Wagner <mishajw@gmail.com>
2016-03-13 21:10:33 +0000

    Date is underlined

--- panel/date_widget.py
+++ panel/date_widget.py
@@ -8,6 +8,7 @@ class DateWidget(Widget):
   
   def __init__(self):
     super(DateWidget, self).__init__()
+    self.has_underline = True
 
   def update_text(self):
     f = os.popen("date +'%a %e %l:%M:%S %p'")
