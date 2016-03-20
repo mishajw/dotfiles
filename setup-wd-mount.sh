70ed4d69303c09b7ab800e0657d6ee0014f31bc6
Misha Wagner <mishajw@gmail.com>
2016-03-20 14:05:09 +0000

    Changed WD IP

--- setup-wd-mount.sh
+++ setup-wd-mount.sh
@@ -2,5 +2,5 @@
 
 sudo mkdir -p /mnt/mycloud
 
-sudo mount -t cifs -o user=misha,passwd=,rw,file_mode=0777,dir_mode=0777 //192.168.0.2/misha /mnt/mycloud
+sudo mount -t cifs -o user=misha,passwd=,rw,file_mode=0777,dir_mode=0777 //192.168.0.22/misha /mnt/mycloud
 
