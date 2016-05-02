45a5b2243a68a58a4ae28326767f6142e12f3ecb
Misha Wagner <mishajw@gmail.com>
2016-05-02 17:35:37 +0100

    Made runnable + params to server

--- gpg-chat-server.sh
+++ gpg-chat-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat | xargs -L1 -I{} bash -c 'gpg --encrypt -o- -r mishajw@gmail.com <(echo {}) | base64 -w 0 ; echo' | nc localhost 65000
+cat | xargs -L1 -I{} bash -c 'gpg --encrypt -o- -r $1 <(echo {}) | base64 -w 0 ; echo' | nc $2 $3
