161e2644239dc3194a8cdf1aa197dd857d350472
Misha Wagner <mishajw@gmail.com>
2016-05-03 20:53:01 +0100

    Removed redundant files

--- start-ext-display.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xrandr --addmode $SECOND_DISPLAY $SECOND_DISPLAY_RES
-xrandr --output $SECOND_DISPLAY --mode $SECOND_DISPLAY_RES --above $MAIN_DISPLAY
-$cnf/bspwmrc
-
--- stop-ext-display.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xrandr --output $SECOND_DISPLAY --off
