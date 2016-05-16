a0c5149e1d5e5704f3d2264b57271a3874bddf98
Misha Wagner <mishajw@gmail.com>
2016-05-16 11:21:30 +0100

    Add script and key binding for toggling gaps

---
+++ scripts/toggle-gap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /home/misha/dotfiles/scripts/start/shell.sh
+
+if [[ "$(bspc config -d focused window_gap)" == "0" ]]; then
+  bspc config -d focused window_gap $WINDOW_GAP
+  echo "Adding gap"
+else
+  bspc config -d focused window_gap 0
+  echo "Removing gap"
+fi
+
