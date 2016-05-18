31621c9aa9e3ee36d73ae7b3c75c8115add9624f
Misha Wagner <mishajw@gmail.com>
2016-05-18 20:18:24 +0100

    Add zsh install script

---
+++ scripts/install/zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
