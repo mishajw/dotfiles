614c9d4521eb5afddbfbc2fe75b68e311cd69483
Misha Wagner <mishajw@gmail.com>
2016-05-18 17:16:56 +0100

    Start install scripts

---
+++ scripts/install/install_yaourt.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /tmp
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+
+alias y=yaourt
