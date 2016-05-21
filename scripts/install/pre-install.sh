778ef5c21e05329a89e591f75c6e30b20901550b
Misha Wagner <mishajw@gmail.com>
2016-05-21 10:16:14 +0100

    Create pre-install script

---
+++ scripts/install/pre-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# This script assumes an internet connection and correctly partitioned disks
+#
+
+read -p "Device file to install on: " dev
+
+# Install arch and base packages on device
+pacstrap -i /mnt base base-devel
+
+# Generate FSTAB file
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Done. Run 'arch-chroot /mnt' and run the root script."
+
