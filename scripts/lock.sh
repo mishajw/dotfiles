34cb0eb63271ea919ac78704f2cd4a43cda542e9
Misha Wagner <mishajw@gmail.com>
2016-12-11 16:33:27 +0000

    Update lock script to blur image

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png -scale 25% -blur 0x10 -scale 400% /tmp/screen.png
 convert /tmp/screen.png $imgs/lock.png -gravity center -composite -matte /tmp/screen.png
 
-i3lock -u -i /tmp/screen.png
+i3lock -t -e -f -u -i /tmp/screen.png
+
 rm /tmp/screen.png
 
