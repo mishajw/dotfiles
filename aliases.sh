1f0be58ac299f63f5b89ffaf4aecb2512532f042
Misha Wagner <mishajw@gmail.com>
2017-04-12 20:12:10 +0100

    Refactor display size code

--- aliases.sh
+++ aliases.sh
@@ -86,8 +86,11 @@ mycloud () {
 
 # Displays
 display-start() {
-  xrandr --addmode $SECOND_DISPLAY $SECOND_DISPLAY_RES
-  xrandr --output $SECOND_DISPLAY --mode $SECOND_DISPLAY_RES --above $MAIN_DISPLAY
+  MODE_NAME="${SECOND_DISPLAY_X}x${SECOND_DISPLAY_Y}_${SECOND_DISPLAY_HZ}_3" 
+  echo "Adding mode $MODE_NAME"
+  xrandr --newmode $MODE_NAME `gtf $SECOND_DISPLAY_X $SECOND_DISPLAY_Y $SECOND_DISPLAY_HZ | grep Modeline | sed -E 's/Modeline "[^ ]*"//g'`
+  xrandr --addmode $SECOND_DISPLAY $MODE_NAME
+  xrandr --output $SECOND_DISPLAY --mode $MODE_NAME --above $MAIN_DISPLAY
   $cnf/bspwmrc
 }
 
