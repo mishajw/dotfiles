f6b68a65abbe92d7351d9eb322cceabd47989ad3
Misha Wagner <mishajw@gmail.com>
2017-04-29 11:16:34 +0100

    Fix startx on boot command, cheers DomWilliams0

--- scripts/start/shell.sh
+++ scripts/start/shell.sh
@@ -3,8 +3,6 @@
 source /home/misha/dotfiles/exports.sh
 source /home/misha/dotfiles/aliases.sh
 
-# # Start x if it isn't already started
-# if which startx 1>/dev/null && pgrep Xorg 1>/dev/null; then
-#   startx
-# fi
+# Start x if it isn't already started
+pgrep Xorg 1>/dev/null || ( [ -z "$TMUX" ] && startx)
 
