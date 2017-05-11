20b8e2478891b928d1437884720418659da2d27f
Misha Wagner <mishajw@gmail.com>
2017-05-11 12:52:23 +0800

    Add script for installing collection of fonts

---
+++ scripts/fonts/install-fonts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+all_packages=""
+
+while read font_line; do
+  font_package=$(echo $font_line | awk '{print $1;}')
+  all_packages="$all_packages $font_package"
+done < font-list
+
+echo $all_packages
+
+yaourt -S $all_packages
+
