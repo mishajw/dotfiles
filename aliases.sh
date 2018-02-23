08ca9dd0b95dc966a72cac09d78ec6a687c641a2
Misha Wagner <mishajw@gmail.com>
2018-02-23 12:18:24 +0000

    Add xclip alias

--- aliases.sh
+++ aliases.sh
@@ -159,4 +159,5 @@ opr () { "$@" > /dev/null 2>&1 & disown }
 alias dmenu="dmenu -o 0.8 -fn $MAIN_FONT -h 50 -w 500 -x 680 -y 490"
 alias go-q="tmux attach -t quake"
 alias sshuttlec="sshuttle --dns -r do 0/0"
+alias xc="xclip -selection clipboard"
 
