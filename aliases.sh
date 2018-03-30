dedab044ecfa98e66ad4d64a15039705dddf2642
Misha Wagner <mishajw@gmail.com>
2018-03-30 15:28:53 +0100

    Add sl function to aliases

--- aliases.sh
+++ aliases.sh
@@ -148,6 +148,18 @@ ve-save-reqs() { pipreqs . }
 alias cr="cargo run"
 alias cb="cargo build"
 
+# sl
+sl() {
+  # Taken from: https://gir.st/blog/sl-alt.htm
+  # sl - prints a mirror image of ls. (C) 2017 Tobias Girstmair, https://gir.st/, GPLv3
+  LEN=$(ls "$@" | wc -L) # get the length of the longest line
+
+  ls "$@" | rev | while read -r line
+  do
+    printf "%${LEN}.${LEN}s\\n" "$line" | sed 's/^\(\s\+\)\(\S\+\)/\2\1/'
+  done
+}
+
 # Misc
 alias dua="du -sh *"
 csvview () { column -s, -t < $@ | less -#2 -N -S }
