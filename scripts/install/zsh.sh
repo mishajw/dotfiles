277dc0bcd152b19ae027b662c2caf4863d030d28
Misha Wagner <mishajw@gmail.com>
2018-09-25 14:22:46 +0100

    Add auto suggestions installation to zsh setup

--- scripts/install/zsh.sh
+++ scripts/install/zsh.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Get oh my zsh
+sh -c "$(curl -fsSL \
+  https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Set up syntax highlighting
+git clone \
+  https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Set up auto-suggestions
+git clone \
+  https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
