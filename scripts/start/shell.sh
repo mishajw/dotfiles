185d4a30bbc0aa6561c729175b411b413d6949f8
Misha Wagner <mishajw@gmail.com>
2018-09-25 14:23:20 +0100

    Remove hard coded home directory when starting shell

--- scripts/start/shell.sh
+++ scripts/start/shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source /home/misha/dotfiles/exports.sh
-source /home/misha/dotfiles/aliases.sh
+source $HOME/dotfiles/exports.sh
+source $HOME/dotfiles/aliases.sh
 
 # Start x if it isn't already started
 pgrep Xorg 1>/dev/null || ( [ -z "$TMUX" ] && startx)
