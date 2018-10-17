bca5eabe6e749fbfeaf2c6521abee1c17955836b
Misha Wagner <mishajw@gmail.com>
2018-10-17 17:04:50 +0100

    Clean up scripts

--- scripts/gpg-chat/client.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-if (($# != 3)); then
-	echo "Usage: $0 <recipient> <server ip> <server port>"
-	exit 1
-fi
-
-export REC=$1
-cat | xargs -L1 -I{} bash -c 'gpg --encrypt -o- -r $REC <(echo {}) | base64 -w 0 ; echo' | /usr/bin/ncat "$2" "$3"
