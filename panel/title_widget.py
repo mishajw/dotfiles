5c7b20e6fd60643737fe5c40739eaa9646eb3c1f
Misha Wagner <mishajw@gmail.com>
2018-10-17 17:26:21 +0100

    Fix title widget

--- panel/title_widget.py
+++ panel/title_widget.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 
+import subprocess
+
 from command_widget import CommandWidget
 from panel_help import *
 
 
 class TitleWidget(CommandWidget):
     def __init__(self):
-        super(TitleWidget, self).__init__("xtitle -sf '%s'")
+        print("title init")
+        super(TitleWidget, self).__init__(
+            "xprop -spy -root _NET_ACTIVE_WINDOW")
 
     def handle_output(self, output):
-        self.text = "%{-u}" + output.decode().replace("\n", "")
+        window_id = output.decode().split(" ")[-1]
+
+        window_name = subprocess.run(
+            ["xdotool", "getwindowname", window_id],
+            stdout=subprocess.PIPE).stdout.decode().strip()
+
+        self.text = "%{-u}" + window_name
         register_update()
