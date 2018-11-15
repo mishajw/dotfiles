8445330d49947557ae0c082b6e31d6a8b8c44fbe
Misha Wagner <mishajw@gmail.com>
2018-11-15 19:11:55 +0000

    Add script for starting access point

---
+++ scripts/make_ap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Root required to make AP"
+   exit 1
+fi
+
+rfkill unblock all
+systemctl restart dhcpcd@enp0s25.service
+create_ap -m bridge wlp4s0 enp0s25 mla-wifi mla-wifi -c 9
