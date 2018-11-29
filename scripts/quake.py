a6f9a70335f39db360f9b629d2e27c1a62750570
Misha Wagner <mishajw@gmail.com>
2018-11-29 17:55:10 +0000

    Change quake script to work with any program

--- scripts/quake.py
+++ scripts/quake.py
@@ -4,46 +4,89 @@
 Toggle a drop-down terminal using termite and xdotool
 """
 
-from subprocess import run, call, Popen, DEVNULL
+from subprocess import run, call, Popen, DEVNULL, PIPE
 import time
 import os
+import argparse
+from typing import Optional
 
-TERMINAL_NAME = "QuakeTerminal"
-XDO_CMD = [
-    "xdotool", "search",
-    ]
+DISPLAY = os.environ["DISPLAY"]
 
-# Try to hide the terminal
-hide_result = run(XDO_CMD + [
-    "--onlyvisible", "--classname", TERMINAL_NAME, "windowunmap"])
-if hide_result.returncode == 0:
-    exit()
+def main(tag: str, command: str, window_id_directory: str):
+    if not os.path.isdir(window_id_directory):
+        os.makedirs(window_id_directory)
 
-# If we couldn't hide, then the terminal is already hidden, so we should show
-# the terminal
-show_result = run(XDO_CMD + ["--classname", TERMINAL_NAME, "windowmap"])
-if show_result.returncode == 0:
-    exit()
+    # Get the window ID, creating the window if it doesn't exist
+    window_id = get_window_id(tag, window_id_directory)
+    is_new_window = False
+    if not window_id or not is_window_alive(window_id):
+        print("Creating windows")
+        window_id = create_window(command)
+        is_new_window = True
+        store_window_id(window_id, tag, window_id_directory)
 
-# If we couldn't show, then the terminal must not exist, so we should create it
-Popen([
-    "termite",
-    "--name", TERMINAL_NAME,
-    "--exec", "tmux new -s quake"])
+    if is_window_visible(window_id) and not is_new_window:
+        print("Making window invisible")
+        set_window_visible(window_id, False)
+    else:
+        print("Making window visible")
+        set_window_visible(window_id, True)
+        set_window_geom(window_id)
 
-# Wait until window appears
-while call(
-        XDO_CMD + ["--onlyvisible", "--classname", TERMINAL_NAME],
-        stdout=DEVNULL):
-    pass
+def get_window_id(tag: str, window_id_directory: str) -> Optional[int]:
+    window_id_path = os.path.join(window_id_directory, tag + DISPLAY)
+    if not os.path.isfile(window_id_path):
+        return None
+    with open(window_id_path, "r") as f:
+        return int(next(f))
 
-# TODO: Find workaround for sleep call
-time.sleep(0.2)
+def store_window_id(window_id: int, tag: str, window_id_directory: str):
+    window_id_path = os.path.join(window_id_directory, tag + DISPLAY)
+    with open(window_id_path, "w") as f:
+        f.write(str(window_id))
 
-# Set position and size of terminal
-run(XDO_CMD + [
-    "--classname", TERMINAL_NAME,
-    "windowmove", "0", os.environ["PANEL_HEIGHT"]], check=True)
-run(XDO_CMD + [
-    "--classname", TERMINAL_NAME,
-    "windowsize", "2556", "400"], check=True)
+def is_window_alive(window_id: int) -> bool:
+    return run(["xdotool", "getwindowname", str(window_id)]).returncode == 0
+
+def create_window(command: str) -> int:
+    previous = get_active_window_id()
+    Popen(command.split(" "))
+    while True:
+        current = get_active_window_id()
+        if current != previous:
+            return current
+
+def get_active_window_id() -> int:
+    return int(run(
+        ["xdotool", "getactivewindow"],
+        stdout=PIPE).stdout.decode())
+
+def is_window_visible(window_id: int) -> bool:
+    visible_windows = run(
+        ["xdotool", "search", "--onlyvisible", ".*"],
+        stdout=PIPE).stdout.decode().split("\n")
+    return str(window_id) in visible_windows
+
+def set_window_visible(window_id: int, visible: bool):
+    if visible:
+        run(["xdotool", "windowmap", str(window_id)]);
+    else:
+        run(["xdotool", "windowunmap", str(window_id)]);
+
+def set_window_geom(window_id: int):
+    height = os.environ["PANEL_HEIGHT"]
+    run(["bspc", "node", "-t", "floating"], check=True)
+    run(
+        ["xdotool", "windowmove", str(window_id), "0", height],
+        check=True)
+    run(["xdotool", "windowsize", str(window_id), "2556", "400"], check=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("quake")
+    parser.add_argument("--tag", type=str, required=True)
+    parser.add_argument("--command", type=str, required=True)
+    parser.add_argument("--window-id-directory", type=str,
+        default="/tmp/quake-window-ids")
+    args = parser.parse_args()
+
+    main(args.tag, args.command, args.window_id_directory)
