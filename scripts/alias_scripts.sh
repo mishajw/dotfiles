990bb0088b7106d53b65ae1bbf6d4e81d96831c7
Misha Wagner <mishajw@gmail.com>
2018-12-02 12:31:53 +0000

    Clean up alias_scripts

--- scripts/alias_scripts.sh
+++ scripts/alias_scripts.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
-original_dir=$(pwd)
-cd $(dirname $0)
-
-for file in ./*.{sh,py}; do
-  file_name=$(echo $file | sed 's/[a-z]\+$//g;s/[\./]//g')
-  extension=$(echo $file | grep -Po '[^\.]+$')
-
-  echo Making alias for $file called $file_name
-
+for file in $scr/*.{sh,py}; do
+  file_name="$(echo ${file%.*} | sed 's/.*\///')"
+  extension="${file##*.}"
+  echo "Making alias for $file called $file_name"
   if [ "$extension" = "sh" ]; then
-    alias $file_name="$(realpath $file)"
+    alias $file_name="$file"
   elif [ "$extension" = "py" ]; then
-    alias $file_name="$DF_PYTHON $(realpath $file)"
+    alias $file_name="$DF_PYTHON $file"
   else
     echo "Unrecognized file"
     exit -1
   fi
 done
-
-cd $original_dir
