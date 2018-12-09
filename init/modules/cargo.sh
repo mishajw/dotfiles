790299150b26c1c64d115c285b796bc2b17277ab
Misha Wagner <mishajw@gmail.com>
2018-12-09 08:31:17 -0800

    Add cargo command to setup doc servers

--- init/modules/cargo.sh
+++ init/modules/cargo.sh
@@ -6,3 +6,19 @@ alias cb="c build"
 alias ct="c test"
 alias cf="c +nightly fmt"
 alias cck="c check"
+cdoc() {
+  original_path=$PWD
+
+  cargo doc &&
+    cd target/doc &&
+    python -m http.server 8000 &
+  local_pid=$!
+
+  cd ~/.rustup/toolchains/stable-*/share/doc/rust/html &&
+    python -m http.server 8001 &
+  stdlib_pid=$!
+
+  cd $original_path
+  trap 'kill $local_pid $stdlib_pid; exit' SIGINT
+  wait $local_pid $stdlib_pid
+}
