d9daa16a29d951aef2df94aca90fd51186995dcd
Misha Wagner <mishajw@gmail.com>
2019-04-15 20:04:52 +0100

    Fix mycloud script

--- init/modules/mycloud.sh
+++ init/modules/mycloud.sh
@@ -5,8 +5,8 @@ MYCLOUD_DIRECTORY=/mnt/mycloud
 
 mycloud () {
   echo Mounting mycloud at $MYCLOUD_IP at $MYCLOUD_DIRECTORY
-  sudo mkdir -p $MYCLOUD_DIRECTORY
+  mkdir -p $MYCLOUD_DIRECTORY
   sudo mount --types cifs \
-    --options user=misha,rw,file_mode=0777,dir_mode=0777 \
+    --options user=misha,rw,file_mode=0777,dir_mode=0777,uid=$(id -u),gid=$(id -g) \
     //$MYCLOUD_IP/misha $MYCLOUD_DIRECTORY
 }
