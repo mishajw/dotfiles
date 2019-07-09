6b3a883eee976a7c9dceeb54b418f6f61f3249cd
Misha Wagner <mishajw@gmail.com>
2019-07-09 10:20:47 +0100

    Add function for removing current git branch

--- init/modules/git.sh
+++ init/modules/git.sh
@@ -9,3 +9,23 @@ alias g="git"
     alias "g$final_alias"="git $final_alias"
   done
 }
+g-rm-branch() {
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  if ! git branch | grep -q "master"; then
+    echo "No master branch"
+    return
+  fi
+  if [[ "$current_branch" == "master" ]]; then
+    echo "Can't delete master branch"
+    return
+  fi
+
+  echo "Deleting branch $current_branch"
+  git checkout master \
+    && git branch -D $current_branch
+
+  if git branch --all | grep -q "origin/$current_branch"; then
+    echo "Deleting remote branch $current_branch"
+    git push origin --delete $current_branch
+  fi
+}
