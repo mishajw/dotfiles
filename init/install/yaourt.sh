614393ab7b7e527b67d80789af385c844761c353
Misha Wagner <mishajw@gmail.com>
2019-07-11 20:38:47 +0100

    Refactor yaourt install script

--- init/install/yaourt.sh
+++ init/install/yaourt.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-cd /tmp
-git clone https://aur.archlinux.org/package-query.git
-cd package-query
-makepkg -si
-cd ..
-git clone https://aur.archlinux.org/yaourt.git
-cd yaourt
-makepkg -si
+command -v yaourt 1>/dev/null && return 0
+
+PACKAGE_QUERY=$(mktemp --directory)
+YAOURT=$(mktemp --directory)
+
+git clone https://aur.archlinux.org/package-query.git $PACKAGE_QUERY \
+  && cd $PACKAGE_QUERY \
+  && makepkg --syncdeps --install --needed --noconfirm
+
+git clone https://aur.archlinux.org/yaourt.git $YAOURT \
+  && cd $YAOURT \
+  && makepkg --syncdeps --install --needed --noconfirm
