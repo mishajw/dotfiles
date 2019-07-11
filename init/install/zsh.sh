598a5a39c9047e6140a83baf522df43f71b52561
Misha Wagner <mishajw@gmail.com>
2019-07-11 18:05:27 +0100

    Add check for existing install for oh-my-zsh

--- init/install/zsh.sh
+++ init/install/zsh.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+if [[ -e "$ZSH_CUSTOM" ]]; then
+  exit
+fi
+
 # Get oh my zsh
 sh -c "$(curl -fsSL \
   https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -7,9 +12,9 @@ sh -c "$(curl -fsSL \
 # Set up syntax highlighting
 git clone \
   https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 
 # Set up auto-suggestions
 git clone \
   https://github.com/zsh-users/zsh-autosuggestions \
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
