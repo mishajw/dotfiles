45d4548049ee525f5c4247b93e600777d71a15c0
Misha Wagner <mishajw@gmail.com>
2019-07-21 12:18:49 +0100

    Change super tap detection to listen to all keyboards

--- scripts/incredi-super-tap.py
+++ scripts/incredi-super-tap.py
@@ -7,29 +7,29 @@ import subprocess
 
 SUPER_KEY_ID = 133
 INCREDIC = os.path.join(os.environ["HOME"], ".cargo/bin/incredic")
-KEYBOARD_NAME = os.environ["KEYBOARD_NAME"]
 
-def super_tapped():
-    subprocess.run([INCREDIC, "--command", "show"])
+# Listens to all keypresses from all keyboards
+BASH_COMMAND = """
+xinput list |
+    grep -Po 'id=[0-9]+' |
+    grep -Po '[0-9]+' |
+    xargs --max-procs 100 -I% xinput test %
+"""
 
-def get_keyboard_id() -> int:
-    output = subprocess.check_output(["xinput", "--list"]).decode().split("\n")
-    for line in output:
-        if KEYBOARD_NAME not in line:
-            continue
-        return int(re.search("id=(\d)+", line).group(1))
-    raise AssertionError(f"No keyboard with name {KEYBOARD_NAME} found")
 
-keyboard_id = get_keyboard_id()
 xinput_process = subprocess.Popen(
-    ["xinput", "test", str(keyboard_id)], stdout=subprocess.PIPE)
-last_pressed = None
+    ["bash", "-c", BASH_COMMAND], stdout=subprocess.PIPE
+)
+last_id = None
 for line in io.TextIOWrapper(xinput_process.stdout, encoding="utf-8"):
-    _, mode, key_id = line.strip().split()
-    key_id = int(key_id)
+    _, mode, current_id = line.strip().split()
+    current_id = int(current_id)
 
     if mode == "press":
-        last_pressed = key_id
-    elif mode == "release" and last_pressed == key_id and key_id == 133:
-        super_tapped()
-
+        last_id = current_id
+    elif (
+        mode == "release"
+        and last_id == current_id
+        and current_id == SUPER_KEY_ID
+    ):
+        subprocess.run([INCREDIC, "--command", "show"])
