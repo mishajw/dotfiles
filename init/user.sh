bc96b710259e35ba8b27283ce0784db7d4ea503a
Misha Wagner <mishajw@gmail.com>
2019-07-21 18:33:42 +0100

    Add docker and sudo group to user.sh

--- init/user.sh
+++ init/user.sh
@@ -9,8 +9,14 @@ if [[ "$#" != "1" ]]; then
 fi
 MK_USER=$1
 
-if getent passwd | grep -q $MK_USER; then
-  exit 0
+if ! getent passwd | grep -q $MK_USER; then
+  # Create the user with a group and home directory
+  useradd --user-group --create-home ${MK_USER}
 fi
-# Create the user with a group and home directory
-useradd --user-group --create-home ${MK_USER}
+# Add to docker group to run docker commands
+getent group docker || groupadd docker
+usermod --append --groups docker misha
+# Disable initial lecture to help with scripting
+echo "Defaults: al !lecture" >> /etc/sudoers
+# Add to sudo
+echo "$MK_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
