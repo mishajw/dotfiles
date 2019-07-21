779cabc32ceef7bec36ff895b3724515d5ba43fe
Misha Wagner <mishajw@gmail.com>
2019-07-21 19:58:32 +0100

    Remove zsh install from install.sh

--- init/install.sh
+++ init/install.sh
@@ -5,7 +5,6 @@ set -e
 $init/install/yay.sh
 yay --noconfirm --needed -S $(cat $init/packages/arch | grep -Pv '^#')
 
-$init/install/zsh.sh
 $init/install/python.sh
 $init/install/vim.sh
 $init/install/rustup.sh
