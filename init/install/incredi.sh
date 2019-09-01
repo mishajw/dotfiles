adc39a50b1664a5a90ce3998c2831a35016c5660
Misha Wagner <mishajw@gmail.com>
2019-09-01 12:38:07 +0100

    Use yay to install csdml in incredi install script

--- init/install/incredi.sh
+++ init/install/incredi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 command -v incredi 1>/dev/null && exit 1
-pacman --needed --noconfirm -S csfml
+yay --needed --noconfirm -S csfml
 cargo install --git https://github.com/mishajw/incredi-panel.git
