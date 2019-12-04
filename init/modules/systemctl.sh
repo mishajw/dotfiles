57ab9f4c358f4d9a3fc2deea2089b1c3db69df0a
Misha Wagner <mishajw@gmail.com>
2019-12-04 15:45:59 +0000

    Simplify systemctl aliases.

--- init/modules/systemctl.sh
+++ init/modules/systemctl.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 command -v systemctl 1>/dev/null || return 0
-alias sysstart="sudo systemctl start"
-alias sysstop="sudo systemctl stop"
-alias sysres="sudo systemctl restart"
+alias sc="sudo systemctl"
