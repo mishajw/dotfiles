639813f89aa01137f821058c3d06e47395568cad
Misha Wagner <mishajw@gmail.com>
2023-12-15 10:07:54 +0000

    Add Yabai for tiling window management.

--- init/install.sh
+++ init/install.sh
@@ -2,4 +2,8 @@
 set -e
 $init/install/vim.sh
 brew bundle --file $df/config/Brewfile
+python3 scripts/make-symlinks.py --symlinks config/symlinks.json
 chsh -s $(which zsh)
+
+yabai --start-service
+skhd --start-service
